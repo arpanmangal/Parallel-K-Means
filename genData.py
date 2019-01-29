2a59452eee3b1667b86fd190489875ea2c05c5ad
arpanmangal <mangalarpan@gmail.com>
2019-01-29 18:59:49 +0530

    random partition

--- genData.py
+++ genData.py
@@ -18,9 +18,9 @@ def generateData ():
     Generate data for k-means
     """
     # print (np.random.randint(1000, size=(10, 3)))
-    k = 10 # 10 clusters
-    n = 100 # 100 points
-    var = 50
+    k = 15 # 10 clusters
+    n = 80 # 100 points
+    var = 60
     ulimit = 1000 # 0-1000 range
 
     centroids = np.random.randint(ulimit, size=(k, 3))
@@ -34,12 +34,12 @@ def generateData ():
 
     np.random.shuffle(points)
 
-    dataFile = open('data.txt', 'w')
+    dataFile = open('data3.txt', 'w')
     dataFile.write('%d %d\n' % (n*k, k))
     for point in points:
         dataFile.write('%d %d %d\n' % (point[0][0], point[0][1], point[0][2]))
 
-    resultFile = open('result.txt', 'w')
+    resultFile = open('result3.txt', 'w')
     resultFile.write('%d %d\n' % (n*k, k))
     for point in points:
         resultFile.write('%d %d %d %d\n' % (point[0][0], point[0][1], point[0][2], point[1]))
@@ -79,4 +79,4 @@ def plotData (resultFile="result.txt"):
 
 if __name__ == '__main__':
     # generateData()
-    plotData()
+    plotData('result3.txt')
